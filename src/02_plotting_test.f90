cd53b999f5abdfe50a3cb8b9e7d04a3c2c5227bc
Scott Christensen <scott@christensensoftware.com>
2025-11-22 16:50:56 -0500

    vendorizing submodules

---
+++ src/02_plotting_test.f90
@@ -0,0 +1,36 @@
+!-------------------------------------------------------------------------------
+! M. Scott Christensen (meowFlute)
+!
+! Just testing that the fplot library compiled in the submodule links up as I
+! expect
+!
+! originally written: Saturday 22 November 2025
+!       last revised: Saturday 22 November 2025
+program plot_test
+    use, intrinsic :: iso_fortran_env
+    use fplot_core
+    implicit none
+    
+    ! Local Variables
+    integer(int32), parameter :: npts = 1000
+    real(real64), parameter :: pi = 2.0d0 * acos(0.0d0)
+    real(real64) :: t(npts), x(npts)
+    type(plot_polar) :: plt
+    type(plot_data_2d) :: pd
+    
+    ! Create a function to plot
+    t = linspace(-2.0d0 * pi, 2.0d0 * pi, npts)
+    x = t * sin(t)
+    
+    ! Plot the function
+    call plt%initialize()
+    call plt%set_font_size(14)
+    call plt%set_title("Polar Plot Example")
+    call plt%set_autoscale(.false.)
+    call plt%set_radial_limits([0.0d0, 6.0d0])
+    
+    call pd%define_data(t, x)
+    call pd%set_line_width(2.0)
+    call plt%push(pd)
+    call plt%draw()
+end program
